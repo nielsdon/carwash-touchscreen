27c6c0acf6ef217cf65b1280e7922c438ed573e5
ND <2531786+nielsdon@users.noreply.github.com>
2024-09-23 16:58:54 +0200

    show splash screen during boot

--- update.sh
+++ update.sh
@@ -40,7 +40,12 @@ chmod a+x *.sh
 
 # update and start influxDB reporting
 sudo cp ./get_cpu_temp.sh /usr/local/bin/.
+sudp cp ./asplashscreen.sh /etc/init.d/asplashscreen
+sudo chmod a+x /etc/init.d/asplashscreen
+sudo cp ./asplashscreen.service /etc/systemd/system/.
 sudo chmod a+x /usr/local/bin/get_cpu_temp.sh
 sudo chown telegraf:telegraf /usr/local/bin/get_cpu_temp.sh
 sudo cp telegraf.conf /etc/telegraf/.
 sudo systemctl restart telegraf
+sudo systemctl daemon-reload
+sudo systemctl enable asplashscreen.service

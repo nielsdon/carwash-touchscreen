8bb382e437e732a14e95ca0b1526018c07d606eb
ND <2531786+nielsdon@users.noreply.github.com>
2024-11-18 15:24:46 +0100

    removed spaces on empty line

--- screens/finished.py
+++ screens/finished.py
@@ -7,7 +7,7 @@ class Finished(Screen):
     def on_enter(self, *args, **kwargs):
         # wait 10 seconds before showing the start screen again
         Clock.schedule_once(self.change_screen, 20)
-    
+
     def change_screen(self, _):
         app = App.get_running_app()
         app.show_start_screen()
